00224dd3d1bd0ee8aa2075c51d83b00c65f3fa34
iwana994994 <iwana994994@gmail.com>
2026-01-29 15:20:45 +0100

    fixing route and controllers

--- backend/src/routes/ProblemsRoute.js
+++ backend/src/routes/ProblemsRoute.js
@@ -1,7 +1,7 @@
 import { Router } from "express"
-import {ProblemsController} from "../controllers/ProblemController.js"
+import { allProblems } from "../controllers/ProblemController.js";
 const route = Router()
 
-route.get("/allProblems",ProblemsController)
+route.get("/allProblems",allProblems)
 
 export default route
