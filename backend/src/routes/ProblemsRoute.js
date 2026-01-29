70e3dd9b37a417503d910207ac259443c51af351
iwana994994 <iwana994994@gmail.com>
2026-01-29 15:11:39 +0100

    fixing controller

--- backend/src/routes/ProblemsRoute.js
+++ backend/src/routes/ProblemsRoute.js
@@ -1,5 +1,5 @@
 import { Router } from "express"
-import ProblemsController from "../controllers/ProblemController"
+import ProblemsController from "../controllers/ProblemController.js"
 const route = Router()
 
 route.get("/allProblems",ProblemsController)
